17453482f727c2a5c9dcd9d2334eb8cf4fde9117
hzzheng <zhchaozju@gmail.com>
2018-01-01 11:36:10 +0800

    add code highlight

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
 import cls from './layout.module.scss'
+
+const active = { activeStyle: { backgroundImage: 'none' } }
 
 export default ({ children }) => (
   <div className={cls.app}>
+    <Helmet>
+      <title>Cnote</title>
+      <link
+        rel="stylesheet"
+        href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css"
+      />
+      <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js" />
+    </Helmet>
     <header>
-      <h1>Cnote</h1>
+      <h1>
+        <Link to="/">
+          Cnote
+        </Link>
+      </h1>
       <ul className={cls.nav}>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" exact {...active}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/blog">Blog</Link>
+          <Link to="/blog" {...active}>
+            Blog
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about" {...active}>
+            About
+          </Link>
         </li>
       </ul>
     </header>
--- src/templates/post.js
+++ src/templates/post.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import cls from './templates.module.scss'
 
-export default ({ data }) => {
-  const post = data.markdownRemark;
-  return (
-    <div className={cls.post}>
-      <h2>{post.frontmatter.title}</h2>
-      <div dangerouslySetInnerHTML={{ __html: post.html }} />
-    </div>
-  );
-};
+export default class extends Component {
+  componentDidMount() {
+    this.highlight()
+  }
+
+  highlight() {
+    setTimeout(() => {
+      if (window.hljs) hljs.initHighlighting()
+    }, 1000)
+  }
+
+  render() {
+    const { data } = this.props
+    const post = data.markdownRemark
+    return (
+      <div className={cls.post}>
+        <h2>{post.frontmatter.title}</h2>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    )
+  }
+}
 
 export const query = graphql`
   query BlogPostQuery($slug: String!) {
