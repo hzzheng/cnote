c21b5fae6cba317e45f4173a9af575b50b46a366
hzzheng <zhchaozju@gmail.com>
2018-04-07 19:52:51 +0800

    chore: update

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import ReactDisqusComments from 'react-disqus-comments'
 import cls from './templates.module.scss'
 
 export default ({ data }) => {
@@ -8,14 +7,6 @@ export default ({ data }) => {
     <div className={cls.post}>
       <h2>{post.frontmatter.title}</h2>
       <div className={cls.content} dangerouslySetInnerHTML={{ __html: post.html }} />
-
-      {/* <ReactDisqusComments
-        shortname="chuguan"
-        identifier={post.frontmatter.title}
-        title={post.frontmatter.title}
-        url={`https://chuguan.me${post.fields.slug}`}
-        category_id={post.frontmatter.title}
-      /> */}
     </div>
   )
 }
